4635dce78a879c2a61311841a714b6abf9658ac1
Giovanni Gutierrez <giovanniagut@gmail.com>
2025-11-02 11:15:29 -0700

    deleted fill register (dealt with outside of module) and added kernels

--- image_kernel_conv.v
+++ image_kernel_conv.v
@@ -4,13 +4,15 @@ module conv33 #(
 )(
     input  wire                   clk,  // one tick  per pixel
     input  wire                   rst_n,
-    \\ thre input pixels (per row)
+    \\ three input pixels (1 per row)
     input  wire signed [PIXEL_WIDTH-1:0] pix_top, // 8-bit pixel input (for single channel color)
     input  wire signed [PIXEL_WIDTH-1:0] pix_mid, 
     input  wire signed [PIXEL_WIDTH-1:0] pix_bot, 
     
     input  wire [1:0]             mode,      // 0=sharpen, 1=gaussian blur, 2=edge detection
+    
     output reg  signed [PIXEL_WIDTH-1:0] pixel_out
+    
 );
 
     // 3x3 window to apply kernel to 
@@ -25,16 +27,32 @@ module conv33 #(
     reg signed [PIXEL_WIDTH-1:0] m0, m1;     // row 1: col0, col1
     reg signed [PIXEL_WIDTH-1:0] b0, b1;     // row 2: col0, col1
 
-    // fill state: provides information about how many columns are present in the current window 
-    reg [1:0] fill_count;  // has up to 3 columns
-
     // sign-extension into accumulator to hold large magnitude convolution sums  
     function automatic signed [ACCW-1:0] sx;
        input signed [PIXEL_WIDTH-1:0] v;
        sx = $signed({{(ACCW-PIXEL_WIDTH){v[PIXEL_WIDTH-1]}}, v});
-   endfunction
+    endfunction
 
+    // Writing the three convolution kernels
 
+    // Sharpen: [ 0 -1 0; -1 5 -1; 0 -1 0 ]
+    wire signed [ACCW-1:0] conv_sharpen =
+        (5 * sx(m1)) - ( sx(t1) + sx(m0) + sx(pix_mid) + sx(b1) );
+
+   // Gaussian: [1 2 1; 2 4 2; 1 2 1] * 1/16
+   wire signed [ACCW -1:0] gauss_sum =
+        sx(t0) + ((2*sx(t1)) + sx(pix_top)
+      + (2*sx(m0)) + (4*sx(m1)) + (2*sx(pix_mid))
+      + sx(b0) + (2*sx(b1)) + sx(pix_bot);
+   
+   wire signed [ACCW-1:0] conv_gaussian = gauss_sum >>> 4; // arithmetic right shift by 4: divide by 2^4
+
+   // Edge detection: [-1 -1 -1; -1 8 -1; -1 -1 -1]
+   wire signed [ACCW-1:0]
+        8 * sx(m1)
+      - ( sx(t0) + sx(t1) + sx(pix_top)
+        + sx(m0)          + sx(pix_mid)
+        + sx(b0) + sx(b1) + sx(pix_bot) );
 
     // Function to clip result to 8-bit unsigned
     function signed [PIXEL_WIDTH-1:0] saturate_8bit;
