69e874801d6a26c098f109c6bf9e67c013b03799
Jacob Jelovich <u1411462@umail.utah.edu>
2025-11-02 17:12:05 -0700

    Fix: Changed syntax for 'x default case and removed redundant comments.

--- ALU_32.sv
+++ ALU_32.sv
@@ -13,39 +13,29 @@ module ALU_32(
 
   always@(*)
   begin
-    //Defaults
     result = 32'b0;
 
-    //Operating based on control input
     case(alu_op)
 
-      //ADD control
       ALU_ADD: result = operand_a + operand_b;
 
-      //SUB control
       ALU_SUB: result = operand_a - operand_b;
 
-      //SLL control
       ALU_SLL: result = operand_a << operand_b[4:0]; //only uses lower 5 bits of operand_b since we can shift a max of 32 bits
 
-      //XOR control
       ALU_XOR: result = operand_a ^ operand_b;
 
-      //SRL control
       ALU_SRL: result = operand_a >> operand_b[4:0]; //only uses lower 5 bits of operand_b since we can shift a max of 32 bits
 
-      //SRA control
       ALU_SRA: result = $signed(operand_a) >>> operand_b[4:0]; //preserves the sign bit and also only uses lower 5 bits of operand_b
       //since we can shift a max of 32 bits
 
-      //OR control
       ALU_OR: result = operand_a | operand_b;
 
-      //AND control
       ALU_AND: result = operand_a & operand_b;
 
       //Default case
-      default: 	result = x';
+      default: 	result = 'x;
 
     endcase
 
