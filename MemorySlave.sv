f65bb4099bf8ee82a1c8d0b4a781f323c8b40d6b
JT1a-a <143207190+JT1a-a@users.noreply.github.com>
2025-11-30 16:50:56 -0700

    Mainstreamed a bit of the logic in Memory Slave, ordering and sequential logic altered

--- MemorySlave.sv
+++ MemorySlave.sv
@@ -1,4 +1,5 @@
 `timescale 1ns/1ps
+`include "definitions.vh"
 
 // MARKER_ABCDEFG
 module MemorySlave #(
@@ -14,15 +15,10 @@ module MemorySlave #(
     input  logic [31:0] HWDATA,
     output logic [31:0] HRDATA,
     output logic        HREADY,
-<<<<<<< Updated upstream
-    output logic [1:0]  HRESP,
-    input  logic        HSEL
-=======
     output logic  [1:0] HRESP,
     input  logic        HSEL,
     output integer      num_instr,
     output logic        inst_loaded
->>>>>>> Stashed changes
 );
 
     typedef enum logic [1:0] {
@@ -32,9 +28,6 @@ module MemorySlave #(
         SEQ    = 2'b11
     } htrans_t;
 
-<<<<<<< Updated upstream
-    logic [31:0] mem [0:MEM_SIZE-1];
-=======
     // Memory declaration
     logic [31:0] mem [0:MEM_SIZE/4-1];
 
@@ -57,7 +50,6 @@ module MemorySlave #(
         inst_loaded = 1;
         $display("Instruction memory loaded");
     end
->>>>>>> Stashed changes
 
     logic [$clog2(LATENCY+1)-1:0] latency_cnt;
     logic        busy;
@@ -80,63 +72,21 @@ module MemorySlave #(
             wdata_reg    <= 0;
             HRDATA       <= 0;
 			end else begin
-             // BUSY: countdown latency
+             // busy now
              if (latency_cnt != 0) begin
                     latency_cnt <= latency_cnt - 1;
              end else begin
-				      // Transfer completes now
+				      // Transfer complete
                   if (write_reg)
                       mem[addr_reg[9:2]] <= wdata_reg;
 
-<<<<<<< Updated upstream
-            // capture request
-            addr_reg  <= HADDR;
-            write_reg <= HWRITE;
-            wdata_reg <= HWDATA;
-        end 
-		  
-		  else if (busy) begin
-            
-				if (latency_cnt > 0)
-                latency_cnt <= latency_cnt - 1;
-            else begin
-                // Complete transfer
-                if (write_reg)
-                    mem[addr_reg[9:2]] <= wdata_reg;
-                // For SEQ, increment address
-                if (!write_reg && HTRANS == SEQ)
-                    addr_reg <= addr_reg + 4;
-			    busy <= 0;
-=======
-                  busy <= 0;  // Now we deassert busy, meaning HREADY=1 next cycle
+                  busy <= 0;// HREADY must be ready next cycle
                 end
->>>>>>> Stashed changes
             end
         end
     end
 
     logic [31:0] word = mem[addr_reg[$clog2(MEM_SIZE)-1:2]];
-
-<<<<<<< Updated upstream
-always_comb begin 
-automatic logic [31:0] word = mem[addr_reg[$clog2(MEM_SIZE)-1:2]];
-        case (HSIZE)
-		  // word based addressing (help from GPT)
-				3'b000: case (addr_reg[1:0])
-				
-					2'b00: HRDATA = {24'b0, word[7:0]};
-					2'b01: HRDATA = {24'b0, word[15:8]};
-					2'b10: HRDATA = {24'b0, word[23:16]};
-					2'b11: HRDATA = {24'b0, word[31:24]};
-                    
-			   endcase
-            3'b001: HRDATA = addr_reg[1] ? {16'b0, word[31:16]} : {16'b0, word[15:0]};
-            default: HRDATA = word;
-        endcase
-    end
-    assign HREADY = !busy;
-	 
-=======
     always_comb begin
         case (HSIZE)
             3'b000: begin
@@ -155,7 +105,6 @@ automatic logic [31:0] word = mem[addr_reg[$clog2(MEM_SIZE)-1:2]];
     end
 
     assign HREADY = !busy;
->>>>>>> Stashed changes
     assign HRESP  = 2'b00;
 
 endmodule
